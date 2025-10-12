3fd72bde57f9dd489b8a76b672dfbaffba13c772
jmsarinana94 <jmsarinana94@gmail.com>
2025-10-11 21:06:28 -0600

    chore(api): add categories module and schema sync

---
+++ apps/api/src/modules/categories/dto/payloads.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
+}
+
+export class UpdateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
+}
